c72f594e38d8cfe3bb9dabe1954366a241976a3d
Woori <rose5307@naver.com>
2021-12-09 22:52:58 +0900

    css mobile version navbar modify

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ document.addEventListener('scroll', () => {
     }
 });
 
+
 //Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event)=>{
@@ -20,7 +21,14 @@ navbarMenu.addEventListener('click', (event)=>{
     if(link === null){
         return;
     }
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
+});
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
 });
 
 //Handle scrolling when tapping on the contact me button
